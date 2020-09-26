36dc17eee49d3c992b4d8eb3b1da721764330a71
Andy Lin <andylinnz79@gmail.com>
2020-09-26 20:49:13 +1000

    xd

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,29 +1,78 @@
-import React, { Component } from 'react'
-import '../App.css'
+import React, { useState, useEffect, useRef } from "react";
 
-export default class Search extends Component {
-    state = {
-        address: ''
-    }
-    onChange = (e) => this.setState({ address: e.target.value });
-    onSubmit = (e) => {
-        e.preventDefault();
-        this.props.searchAddress(this.state.address);
-        this.setState({address: ''});
-    }
-    render() {
-        return (
-            <form onSubmit={this.onSubmit}>
-                <input
-                    type="text"
-                    style={inputStyle}
-                    placeholder="Enter or choose your location here"
-                    value={this.state.address}
-                    onChange={this.onChange}
-                />
-            </form>
-        )
-    }
+let autoComplete;
+
+const loadScript = (url, callback) => {
+  let script = document.createElement("script");
+  script.type = "text/javascript";
+
+  if (script.readyState) {
+    script.onreadystatechange = function() {
+      if (script.readyState === "loaded" || script.readyState === "complete") {
+        script.onreadystatechange = null;
+        callback();
+      }
+    };
+  } else {
+    script.onload = () => callback();
+  }
+
+  script.src = url;
+  document.getElementsByTagName("head")[0].appendChild(script);
+};
+
+function handleScriptLoad(updateQuery, autoCompleteRef) {
+  autoComplete = new window.google.maps.places.Autocomplete(
+    autoCompleteRef.current,
+    { types: [], componentRestrictions: { country: "au" } }
+  );
+  autoComplete.setFields(["address_components", "formatted_address"]);
+  autoComplete.addListener("place_changed", () =>
+    handlePlaceSelect(updateQuery)
+  );
+}
+
+async function handlePlaceSelect(updateQuery) {
+  const addressObject = autoComplete.getPlace();
+  const query = addressObject.formatted_address;
+  updateQuery(query);
+}
+
+function Search() {
+  const [query, setQuery] = useState("");
+  const [finalQuery, setFinalQuery] = useState("");
+  const autoCompleteRef = useRef(null);
+
+  useEffect(() => {
+    loadScript(
+      `https://maps.googleapis.com/maps/api/js?key=AIzaSyChqFfRblEdYBYRzlurWG2QTtbgIjVl9AQ&libraries=places`,
+      () => handleScriptLoad(setQuery, autoCompleteRef)
+    );
+  }, []);
+
+  useEffect(() => {
+    setFinalQuery(query);
+    console.log(query);
+  }, [query]
+  )
+
+  const onSubmit = (e) => {
+      e.preventDefault();
+      return false;
+  }
+
+    return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        style={inputStyle}
+        ref={autoCompleteRef}
+        onChange={event => setQuery(event.target.value)}
+        placeholder="Enter or choose your location here"
+        value={query}
+      />
+    </form>
+  );
 }
 
 const inputStyle = {
@@ -33,3 +82,5 @@ const inputStyle = {
     height: "60px",
     margin: "10px",
 }
+
+export default Search;
