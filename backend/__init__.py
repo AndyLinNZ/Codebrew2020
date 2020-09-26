f3ff099aed693bb8843de7df26909b4f457c1b86
rakshith48 <33714643+rakshith48@users.noreply.github.com>
2020-09-26 08:31:13 +0530

    Add files via upload

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,1 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from CodeBrew.nearby_hospitals import nearby_hospitals
+
+app = Flask(__name__)
+
+@app.route('/')
+def index(location):
+    
+    hospitals = nearby_hospitals(location)
+    return render_template('index.html', data = hospitals)
+
+app.run()
