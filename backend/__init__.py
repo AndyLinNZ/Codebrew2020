3e0354153e81878ce092b792955c9f6ea9c57615
Stefan Armando Marbun <StefanMarbun@192-168-1-107.tpgi.com.au>
2020-09-26 15:17:03 +1000

    added registration form data

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,1 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, flash, request, url_for, redirect, session
+from flask_mail import Mail, Message
+from wtforms import Form, BooleanField, TextField, PasswordField, validators
+import gc
+
+app = Flask(__name__)
+
+class RegistrationForm(Form):
+    fullname = TextField('Full Name', [validators.Length(min=4, max=100)])
+    email = TextField('Email Address', [validators.Length(min=6, max=50)])
+    dob = DateField('Date of Birth', format='%d/%m/%Y')
+    phone = TextField('Phone Number', [validators.Length(min=7, max=12)])
+    appointmentTime = DateTimeField('Appointment Time', format='%d/%m/%Y/%H/%M')
+
+
+@app.route('/register/', methods=["POST"])
+def register_page():
+    try:
+        form = RegistrationForm(request.form)
+
+        if request.method == "POST" and form.validate():
+            fullname  = form.fullname.data
+            email = form.email.data
+            dob = form.dob.data
+            phone = form.phone.data
+            appointmentTime = form.appointmentTime.data
+            "fullname\n"
+
+    except Exception as e:
+        return(str(e))
