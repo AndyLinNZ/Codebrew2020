a00573d4f585321a9f427dc1f519ab57e0ce06e0
JoshDubar <joshuadubar123@gmail.com>
2020-09-26 22:15:50 +1000

    form disappear after submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Map from './components/Map';
 import Home from './components/layouts/Home';
 
 function App() {
+  
+  const [location, setLocation] = useState([])
+
+  const onFormSubmit = point => {
+    setLocation(point)
+  }
+
   return (
     <div className="App">
       <Map/>
-      <Home/>
+      {location.length !== 2 && <Home onFormSubmit={onFormSubmit}/>}
     </div>
   );
 
