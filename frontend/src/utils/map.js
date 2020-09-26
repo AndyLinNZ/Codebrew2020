121c6c813af04f6edba60a0bd7596e8488af1dbf
JoshDubar <joshuadubar123@gmail.com>
2020-09-27 02:48:50 +1000

    holy shit i'm done

--- frontend/src/utils/map.js
+++ frontend/src/utils/map.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import mapboxgl from 'mapbox-gl'
-import UserMarker from '../components/Markers/UserMarker'
-import HospitalMarker from '../components/Markers/HospitalMarker'
 import { green, blue } from '@material-ui/core/colors'
 import { LocalHospital, Home } from '@material-ui/icons';
+import HospitalPopup from '../components/HospitalPopup'
 
 export const addMarkers = ({
   map,
@@ -16,11 +15,11 @@ export const addMarkers = ({
   }
   hospitalData.forEach((hospital) => {
     const coordinates = [hospital.geometry.location.lng, hospital.geometry.location.lat]
+    const { formatted_address: address, formatted_phone_number: phoneNumber, name, opening_hours: openingHours,  vaccineLevel , website } = hospital
     const hospitalMarker = createMarker(<LocalHospital style={{ fontSize: 40, color: green[300], cursor: 'pointer' }}/>, { anchor: 'bottom' })
     hospitalMarker.setLngLat(coordinates)
-    hospitalMarker.setPopup(new mapboxgl.Popup().setHTML(
-      `<h3>Hello</h3>`
-    ))
+    const popup = createPopup(<HospitalPopup vaccineLevel={vaccineLevel} address={address} phoneNumber={phoneNumber} name={name} website={website} openingHours={openingHours}/>)
+    hospitalMarker.setPopup(popup)
     hospitalMarker.addTo(map)
   })
   if (location.length === 2) {
@@ -29,6 +28,12 @@ export const addMarkers = ({
     userMarker.addTo(map)
     map.flyTo({center: location, zoom: 13})
   }
+}
+
+const createPopup = (component) => {
+  const popup = document.createElement('div')
+  ReactDom.render(component, popup)
+  return new mapboxgl.Popup().setDOMContent(popup)
 }
 
 const createMarker = (component, options) => {
